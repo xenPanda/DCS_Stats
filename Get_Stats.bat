8b2e7c3a977bf0fe5df051b1847a1fb2a0be6605
xenPanda <ctbarilla@gmail.com>
2020-10-09 14:23:48 -0700

    Update Get_Stats.bat

--- Get_Stats.bat
+++ Get_Stats.bat
@@ -1,3 +1,11 @@
+Rem DCS_Stats - DCS stats collection via Slmod and insert into MySql DB
+
+Rem Author: Chris Barilla (Panda)
+Rem Date:   09/05/2020
+
+Rem Filename: Get_Stats.bat
+Rem Version: 0.0.1
+
 @echo off
 
 "C:\Users\root\AppData\Local\Programs\Python\Python38\python.exe" "C:\Users\root\Desktop\scripts\stat_collector.py"
